23270fc5f4bd668405f4aab833ff38c618e8e042
ataraxia7899 <ataraxia7899@gmail.com>
2024-10-15 22:50:33 +0900

    1차 프로젝트 완료 나중에 검토 필요

---
+++ todo_list/App.jsx
@@ -0,0 +1,16 @@
+import React,{useState} from 'react';
+
+//1. 인풋창과 버튼을 생성
+//2. 인풋창에 값을 입력 후 버튼을 클릭 시 아이템이 추가됨
+//3. 아이템 삭제버튼을 누르면 삭제도 가능.
+
+function App() {
+    
+    return (
+        <main>
+            <input type="text"/>
+            <button>추가</button>
+
+        </main>
+    )
+}
