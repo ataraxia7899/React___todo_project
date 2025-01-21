8d9b61d7915234b6e7ba24a00e20dbde023fe27d
ataraxia7899 <ataraxia7899@gmail.com>
2025-01-21 16:47:12 +0900

    파일정리 + Readme 내용 정리리

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,6 @@ import React, {useState} from 'react'
 import './App.css'
 import TodoBoard from "./components/TodoBoard"
 
-//1. 인풋창과 버튼을 생성
-//2. 인풋창에 값을 입력 후 버튼을 클릭 시 아이템이 추가됨
-
-//3. 아이템 삭제버튼을 누르면 삭제도 가능.  -> 처음 하는 리엑트 프로젝트라서 아이디어는 있는데 코드를 짜지 못함.
-
 function App() {
   const [inputValue, setinputValue] = useState('')    
   const [todoList, setTodoList] = useState([])
